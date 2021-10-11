6e64ba261a50456ce15e07e80daa90e06f657fc8
StanSixx <82747581+StanSixx@users.noreply.github.com>
2021-10-11 11:55:55 -0600

    Revert "Revert "Modificación forms""
    This reverts commit fa62d06c8760cc9f51d025a49e0c86c16922910c.

---
+++ CustomControls/CategoriaCard.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockIt.CustomControls
+{
+    public partial class CategoriaCard : UserControl
+    {
+        public CategoriaCard()
+        {
+            InitializeComponent();
+        }
+
+        private String catProd;
+        private Button btnEditarProp;
+        private Button btnEliminarProp;
+
+        
+        [Category("Custom Props")]
+        public String CatProd
+        {
+            get { return catProd; }
+            set { catProd = value; this.lblCatProd.Text = value; }
+        }
+                
+        [Category("Custom Props")]
+        public Button BtnEditarProp
+        {
+            get { return btnEditarProp; }
+            set { btnEditarProp = value; this.btnEditar = value; }
+        }
+
+        [Category("Custom Props")]
+        public Button BtnEliminarProp
+        {
+            get { return btnEliminarProp; }
+            set { btnEliminarProp = value; this.btnEliminar = value; }
+        }
+
+
+        [Browsable(true)]
+        [Category("Action")]
+        [Description("Invocado cuando el usuario hace clic en boton Editar")]
+        public event EventHandler ButtonClickEditar;
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (this.ButtonClickEditar != null)
+                this.ButtonClickEditar(this, e);
+        }
+
+        [Browsable(true)]
+        [Category("Action")]
+        [Description("Invocado cuando el usuario hace clic en boton Eliminar")]
+        public event EventHandler ButtonClickEliminar;
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (this.ButtonClickEliminar != null)
+                this.ButtonClickEliminar(this, e);
+        }
+    }
+}
---
+++ CustomControls/ReservaCard.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace StockIt.CustomControls
+{
+    partial class ReservaCard
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.lblPreProd = new System.Windows.Forms.Label();
+            this.lblNomClie = new System.Windows.Forms.Label();
+            this.lblNomProd = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbxImgRes = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImgRes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnEliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(62)))), ((int)(((byte)(87)))));
+            this.btnEliminar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.ForeColor = System.Drawing.Color.White;
+            this.btnEliminar.Location = new System.Drawing.Point(612, 79);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(98, 32);
+            this.btnEliminar.TabIndex = 24;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnEditar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(117)))), ((int)(((byte)(169)))));
+            this.btnEditar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.ForeColor = System.Drawing.Color.White;
+            this.btnEditar.Location = new System.Drawing.Point(612, 31);
+            this.btnEditar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(98, 32);
+            this.btnEditar.TabIndex = 23;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // lblPreProd
+            // 
+            this.lblPreProd.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblPreProd.AutoEllipsis = true;
+            this.lblPreProd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPreProd.Location = new System.Drawing.Point(218, 79);
+            this.lblPreProd.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPreProd.Name = "lblPreProd";
+            this.lblPreProd.Size = new System.Drawing.Size(375, 19);
+            this.lblPreProd.TabIndex = 22;
+            this.lblPreProd.Text = "Precio";
+            this.lblPreProd.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblNomClie
+            // 
+            this.lblNomClie.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblNomClie.AutoEllipsis = true;
+            this.lblNomClie.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomClie.Location = new System.Drawing.Point(218, 44);
+            this.lblNomClie.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNomClie.Name = "lblNomClie";
+            this.lblNomClie.Size = new System.Drawing.Size(375, 19);
+            this.lblNomClie.TabIndex = 21;
+            this.lblNomClie.Text = "Categoría";
+            this.lblNomClie.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblNomProd
+            // 
+            this.lblNomProd.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblNomProd.AutoEllipsis = true;
+            this.lblNomProd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomProd.Location = new System.Drawing.Point(218, 11);
+            this.lblNomProd.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNomProd.Name = "lblNomProd";
+            this.lblNomProd.Size = new System.Drawing.Size(375, 19);
+            this.lblNomProd.TabIndex = 20;
+            this.lblNomProd.Text = "Producto";
+            this.lblNomProd.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(144, 79);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 19);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Precio:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(144, 44);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 19);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Cliente:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(144, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 19);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Producto:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pbxImgRes
+            // 
+            this.pbxImgRes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbxImgRes.Location = new System.Drawing.Point(15, 11);
+            this.pbxImgRes.Margin = new System.Windows.Forms.Padding(2);
+            this.pbxImgRes.Name = "pbxImgRes";
+            this.pbxImgRes.Size = new System.Drawing.Size(113, 122);
+            this.pbxImgRes.TabIndex = 16;
+            this.pbxImgRes.TabStop = false;
+            // 
+            // ReservaCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.lblPreProd);
+            this.Controls.Add(this.lblNomClie);
+            this.Controls.Add(this.lblNomProd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbxImgRes);
+            this.Name = "ReservaCard";
+            this.Size = new System.Drawing.Size(724, 144);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImgRes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Label lblPreProd;
+        private System.Windows.Forms.Label lblNomClie;
+        private System.Windows.Forms.Label lblNomProd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbxImgRes;
+    }
+}
