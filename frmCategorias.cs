fac95cf104bb8e00d86eda19800dc9b3bc8bed96
StanSixx <82747581+StanSixx@users.noreply.github.com>
2021-10-11 12:07:19 -0600

    Revert "Modificación forms"
    This reverts commit a69d36cb70b277114aea20b453213d42bbc3ac98.

--- frmCategorias.cs
+++ frmCategorias.cs
@@ -1,5 +1,4 @@
-﻿using StockIt.CustomControls;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,49 +16,5 @@ namespace StockIt
         {
             InitializeComponent();
         }
-
-        private void frmCategorias_Load(object sender, EventArgs e)
-        {
-            cargarCategorias();
-        }
-
-        private void cargarCategorias()
-        {
-            CategoriaCard[] categorias = new CategoriaCard[10];
-            for (int i = 0; i < categorias.Length; i++)
-            {
-                categorias[i] = new CategoriaCard();
-                categorias[i].Name = "CategoriaCard" + i.ToString();
-                categorias[i].CatProd = "Categoría" + " " + i.ToString();
-
-                //Creación de btnEditar
-                categorias[i].BtnEditarProp = new Button();
-                categorias[i].ButtonClickEditar += new EventHandler(btnEditar_ButtonClick);
-
-                void btnEditar_ButtonClick(object sender, EventArgs e)
-                {
-                    //Manejar evento
-                    CategoriaCard categoriaCardItem = ((CategoriaCard)sender);
-                    this.txtCategoria.Text = categoriaCardItem.Name + "Editar";
-                }
-
-                //Creación de btnEliminar
-                categorias[i].BtnEliminarProp = new Button();
-                categorias[i].ButtonClickEliminar += new EventHandler(btnEliminar_ButtonClick);
-
-                void btnEliminar_ButtonClick(object sender, EventArgs e)
-                {
-                    //Manejar evento
-                    CategoriaCard categoriaCardItem = ((CategoriaCard)sender);
-                    this.txtCategoria.Text = categoriaCardItem.Name + "Eliminar";
-                    categoriaCardItem.Dispose();
-                }
-
-                //Agregamos el ProductoCard al FlowLAyoutPanel
-                flpListadoCategorias.Controls.Add(categorias[i]);
-            }
-        }
     }
-
-    
 }
--- frmReservas.cs
+++ frmReservas.cs
@@ -1,5 +1,4 @@
-﻿using StockIt.CustomControls;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,48 +17,9 @@ namespace StockIt
             InitializeComponent();
         }
 
-        private void frmReservas_Load(object sender, EventArgs e)
+        private void txtNomMClie_TextChanged(object sender, EventArgs e)
         {
-            cargarReservas();
-        }
 
-        private void cargarReservas()
-        {
-            ReservaCard[] reservas = new ReservaCard[10];
-            for (int i = 0; i < reservas.Length; i++)
-            {
-                reservas[i] = new ReservaCard();
-                reservas[i].Name = "ReservaCard" + i.ToString();
-                reservas[i].NomProd = "Producto " + i.ToString();
-                reservas[i].NomClie = "Cliente " + i.ToString();
-                reservas[i].PreProd = 8.50;
-
-                //Creación de btnEditar
-                reservas[i].BtnEditarProp = new Button();
-                reservas[i].ButtonClickEditar += new EventHandler(btnEditar_ButtonClick);
-
-                void btnEditar_ButtonClick(object sender, EventArgs e)
-                {
-                    //Manejar evento
-                    ReservaCard reservaCardItem = ((ReservaCard)sender);
-                    this.txtReservas.Text = reservaCardItem.Name + "Editar";
-                }
-
-                //Creación de btnEliminar
-                reservas[i].BtnEliminarProp = new Button();
-                reservas[i].ButtonClickEliminar += new EventHandler(btnEliminar_ButtonClick);
-
-                void btnEliminar_ButtonClick(object sender, EventArgs e)
-                {
-                    //Manejar evento
-                    ReservaCard reservaCardItem = ((ReservaCard)sender);
-                    this.txtReservas.Text = reservaCardItem.Name + "Eliminar";
-                    reservaCardItem.Dispose();
-                }
-
-                //Agregamos el ProductoCard al FlowLAyoutPanel
-                flpListadoReservas.Controls.Add(reservas[i]);
-            }
         }
     }
 }
