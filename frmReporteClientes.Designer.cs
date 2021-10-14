09ecb78640159e1b4f9b2257baab74e011b741b5
Sofía Alvarado <alvaradosofiaH@outlook.com>
2021-10-13 22:23:27 -0600

    Agregando frmReporteClientes 2

--- frmReporteClientes.Designer.cs
+++ frmReporteClientes.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace StockIt
+﻿namespace StockIt
 {
     partial class frmReporteClientes
     {
@@ -29,52 +28,12 @@ namespace StockIt
         /// </summary>
         private void InitializeComponent()
         {
-            this.gridClientes = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.gridClientes)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // gridClientes
-            // 
-            this.gridClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridClientes.Location = new System.Drawing.Point(76, 132);
-            this.gridClientes.Name = "gridClientes";
-            this.gridClientes.Size = new System.Drawing.Size(775, 402);
-            this.gridClientes.TabIndex = 33;
-            // 
-            // label1
-            // 
-            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Padding = new System.Windows.Forms.Padding(0, 24, 0, 0);
-            this.label1.Size = new System.Drawing.Size(907, 57);
-            this.label1.TabIndex = 28;
-            this.label1.Text = "Reporte de Clientes";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // frmReporteClientes
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(907, 601);
-            this.Controls.Add(this.gridClientes);
-            this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.KeyPreview = true;
-            this.Name = "frmReporteClientes";
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "frmReporteClientes";
-            ((System.ComponentModel.ISupportInitialize)(this.gridClientes)).EndInit();
-            this.ResumeLayout(false);
-
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView gridClientes;
-        private System.Windows.Forms.Label label1;
     }
 }
