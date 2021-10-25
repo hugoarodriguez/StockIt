79866093dc120a7165bb0c8f5b3c7461d4db5bcb
Hugo Rodríguez <hugsrodriguez8@gmail.com>
2021-10-24 21:03:19 -0600

    Cambios en estilo y agregacion de validaciones al frmCategorias

--- frmCategorias.Designer.cs
+++ frmCategorias.Designer.cs
@@ -31,9 +31,10 @@ namespace StockIt
         {
             this.label2 = new System.Windows.Forms.Label();
             this.txtCategoria = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.btnAgregar = new System.Windows.Forms.Button();
             this.flpListadoCategorias = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label2
@@ -41,36 +42,22 @@ namespace StockIt
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(158, 100);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(211, 140);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(118, 17);
+            this.label2.Size = new System.Drawing.Size(138, 20);
             this.label2.TabIndex = 32;
             this.label2.Text = "Ingrese categoría";
             // 
             // txtCategoria
             // 
             this.txtCategoria.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtCategoria.BackColor = System.Drawing.SystemColors.Window;
+            this.txtCategoria.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(234)))), ((int)(((byte)(246)))));
             this.txtCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCategoria.Location = new System.Drawing.Point(161, 119);
-            this.txtCategoria.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCategoria.Location = new System.Drawing.Point(215, 170);
+            this.txtCategoria.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtCategoria.Name = "txtCategoria";
-            this.txtCategoria.Size = new System.Drawing.Size(461, 26);
+            this.txtCategoria.Size = new System.Drawing.Size(613, 30);
             this.txtCategoria.TabIndex = 31;
-            // 
-            // label1
-            // 
-            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Padding = new System.Windows.Forms.Padding(0, 24, 0, 0);
-            this.label1.Size = new System.Drawing.Size(923, 57);
-            this.label1.TabIndex = 30;
-            this.label1.Text = "Categorías";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // btnAgregar
             // 
@@ -79,36 +66,66 @@ namespace StockIt
             this.btnAgregar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAgregar.ForeColor = System.Drawing.Color.White;
-            this.btnAgregar.Location = new System.Drawing.Point(626, 117);
-            this.btnAgregar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAgregar.Location = new System.Drawing.Point(841, 166);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(98, 32);
+            this.btnAgregar.Size = new System.Drawing.Size(131, 39);
             this.btnAgregar.TabIndex = 34;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // flpListadoCategorias
             // 
             this.flpListadoCategorias.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.flpListadoCategorias.AutoScroll = true;
-            this.flpListadoCategorias.Location = new System.Drawing.Point(86, 170);
-            this.flpListadoCategorias.Margin = new System.Windows.Forms.Padding(2);
+            this.flpListadoCategorias.Location = new System.Drawing.Point(115, 233);
+            this.flpListadoCategorias.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.flpListadoCategorias.Name = "flpListadoCategorias";
-            this.flpListadoCategorias.Size = new System.Drawing.Size(750, 471);
+            this.flpListadoCategorias.Size = new System.Drawing.Size(1000, 580);
             this.flpListadoCategorias.TabIndex = 35;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
+            this.label1.Size = new System.Drawing.Size(1231, 70);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "Categorías";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(62)))), ((int)(((byte)(87)))));
+            this.btnCancelar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.White;
+            this.btnCancelar.Location = new System.Drawing.Point(985, 165);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(130, 40);
+            this.btnCancelar.TabIndex = 37;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // frmCategorias
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(923, 857);
+            this.ClientSize = new System.Drawing.Size(1231, 1102);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.flpListadoCategorias);
             this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtCategoria);
-            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmCategorias";
             this.Text = "frmCategorias";
             this.Load += new System.EventHandler(this.frmCategorias_Load);
@@ -121,8 +138,9 @@ namespace StockIt
 
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtCategoria;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.FlowLayoutPanel flpListadoCategorias;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCancelar;
     }
 }
--- frmCategorias.cs
+++ frmCategorias.cs
@@ -13,6 +13,9 @@ namespace StockIt
 {
     public partial class frmCategorias : Form
     {
+        Utils utils = new Utils();
+        string nombreActualCategoria;
+
         public frmCategorias()
         {
             InitializeComponent();
@@ -21,15 +24,21 @@ namespace StockIt
         private void frmCategorias_Load(object sender, EventArgs e)
         {
             cargarCategorias();
+            btnCancelar.Hide();//Ocultamos el btnCancelar
         }
 
         private void cargarCategorias()
         {
+            if (flpListadoCategorias.Controls.Count > 0)
+            {
+                flpListadoCategorias.Controls.Clear();
+            }
+
             CategoriaCard[] categorias = new CategoriaCard[10];
             for (int i = 0; i < categorias.Length; i++)
             {
                 categorias[i] = new CategoriaCard();
-                categorias[i].Name = "CategoriaCard" + i.ToString();
+                categorias[i].Name = "CategoriaCard" + i.ToString();//Colocar el id de la BD como nombre de la Card
                 categorias[i].Categ = "Categoría" + " " + i.ToString();
 
                 //Creación de btnEditar
@@ -41,6 +50,12 @@ namespace StockIt
                     //Manejar evento
                     CategoriaCard categoriaCardItem = ((CategoriaCard)sender);
                     this.txtCategoria.Text = categoriaCardItem.Name + "Editar";
+
+                    //Posteriormente se puede hacer consultando el nombre a la BD con el ID de la categoría
+                    nombreActualCategoria = this.txtCategoria.Text = categoriaCardItem.Name;//Obtenemos el nombre actual de la categoría
+                    btnAgregar.Text = "Editar";//Cambiamos el texto del btnAgregar
+                    btnCancelar.Show();//Mostramos el btnCancelar, que permitirá cancelar la edición de la categoría
+                    flpListadoCategorias.Hide();//Ocultamos el FlowLayoutPanel de Categorías
                 }
 
                 //Creación de btnEliminar
@@ -52,11 +67,88 @@ namespace StockIt
                     //Manejar evento
                     CategoriaCard categoriaCardItem = ((CategoriaCard)sender);
                     this.txtCategoria.Text = categoriaCardItem.Name + "Eliminar";
+
+                    /*
+                     * Validar si la categoría seleccionada para eliminar tiene productos (con stock o reserva activa)
+                     * relacionados en la BD.
+                     * Si NO tienen ninguno relacionado SI se debe permitir eliminar
+                     */
                     categoriaCardItem.Dispose();
                 }
 
                 //Agregamos el ProductoCard al FlowLAyoutPanel
                 flpListadoCategorias.Controls.Add(categorias[i]);
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+            if (btnAgregar.Text == "Agregar")
+            {
+                if (txtCategoria.Text.Trim() != "")
+                {
+                    string nuevoNombreCategoria = txtCategoria.Text.Trim();
+
+                    //Agregar categoría
+                    utils.messageBoxOperacionExitosa("Se agregó la categoría \""+ nuevoNombreCategoria +"\"");
+
+                    //Cargamos las categorías nuevamente
+                    cargarCategorias();
+
+                    //Limpiamos los controles
+                    limpiarControles();
+                }
+                else
+                {
+                    utils.messageBoxCampoRequerido("Debes ingresar el nombre de la nueva categoría.");
+                    txtCategoria.Focus();
+                }
+            }
+            else
+            {
+                if (txtCategoria.Text.Trim() != "")
+                {
+                    string nuevoNombreCategoria = txtCategoria.Text.Trim();
+
+                    //Modificar categoría
+                    utils.messageBoxOperacionExitosa("La categoría \"" + nombreActualCategoria + "\"" +
+                        " paso a llamarse \""+ nuevoNombreCategoria + "\"");
+
+                    //Cargamos las categorías nuevamente
+                    cargarCategorias();
+
+                    //Limpiamos los controles
+                    limpiarControles();
+                }
+                else
+                {
+                    utils.messageBoxCampoRequerido("Debes ingresar el nuevo nombre de la categoría \""+ nombreActualCategoria +"\".");
+                    txtCategoria.Focus();
+                }
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            limpiarControles();
+        }
+
+        //Permite reestablecer el estado inicial de los controles antes de presionar la opción "Editar" en la Categoría
+        private void limpiarControles()
+        {
+            if (btnAgregar.Text == "Agregar")
+            {
+                txtCategoria.Text = null;
+                txtCategoria.Focus();
+            }
+            else
+            {
+                txtCategoria.Text = null;
+                txtCategoria.Focus();
+                btnAgregar.Text = "Agregar";//Cambiamos el texto del btnAgregar
+                btnCancelar.Hide();//Ocultamos el btnCancelar
+                flpListadoCategorias.Show();//Mostramos el FlowLayoutPanel de Categorías
             }
         }
     }
