821ba714ce29beab727c99d4ebe3693fd4e3ed4c
Hugo Rodríguez <hugsrodriguez8@gmail.com>
2021-10-30 11:53:09 -0600

    Finalización de Acerca De

--- frmAcercaDe.Designer.cs
+++ frmAcercaDe.Designer.cs
@@ -30,6 +30,19 @@ namespace StockIt
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -44,16 +57,137 @@ namespace StockIt
             this.label1.Text = "Acerca De";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(234)))), ((int)(((byte)(246)))));
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(115, 151);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1000, 580);
+            this.panel1.TabIndex = 20;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(94, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(400, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Alvarado Herníquez, Sofía Michelle";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(510, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(400, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "25-3152-2017";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(94, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(812, 23);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Sistema desarrollador por:";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(510, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(400, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "25-3836-2017";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(94, 220);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(400, 23);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Hernández Campos, Samuel Enrique";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(510, 301);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(400, 20);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "25-0369-2017";
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(94, 300);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(400, 23);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Palma Portillo, Bladimir Stanley";
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(510, 381);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(400, 20);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "25-0663-2017";
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(94, 380);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(400, 23);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "Rodríguez Cruz, Hugo Alexander";
+            // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(510, 461);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(400, 20);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "25-0259-2017";
+            // 
+            // label12
+            // 
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(94, 460);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(400, 23);
+            this.label12.TabIndex = 8;
+            this.label12.Text = "Rodríguez Sigüenza, Ámilton Abraham";
+            // 
             // frmAcercaDe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1231, 1102);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmAcercaDe";
             this.Text = "frmAcercaDe";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -61,5 +195,17 @@ namespace StockIt
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
     }
 }
